55af4cd3913c4dd1297711e5ac40c84e4d32b0a6
Atchaiya Babu <atchaiyababu@gmail.com>
2026-07-03 14:48:15 +0530

    Add files via upload

---
+++ Week-1/SingletonPatternExample/SingletonTest.java
@@ -0,0 +1,26 @@
+package com.cognizant.singleton;
+
+public class SingletonTest {
+
+    public static void main(String[] args) {
+
+        Logger logger1 = Logger.getInstance();
+
+        Logger logger2 = Logger.getInstance();
+
+        logger1.log("First Message");
+
+        logger2.log("Second Message");
+
+        System.out.println("Logger1 Address: " + logger1);
+
+        System.out.println("Logger2 Address: " + logger2);
+
+        if(logger1 == logger2) {
+            System.out.println("Only one Logger instance exists.");
+        }
+        else {
+            System.out.println("Multiple instances exist.");
+        }
+    }
+}
