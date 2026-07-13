204054f85582bc937cf5b0a76b7a9760a5ffe130
Atchaiya Babu <atchaiyababu@gmail.com>
2026-07-13 22:27:24 +0530

    Add files via upload

---
+++ Week-3/2. spring-rest-handson-REST - Get country based on country code/src/src_main/CountryController.java
@@ -0,0 +1,27 @@
+package com.cognizant.springlearn.controller;
+
+import com.cognizant.springlearn.model.Country;
+import com.cognizant.springlearn.service.CountryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CountryController {
+
+    @Autowired
+    private CountryService countryService;
+
+    @GetMapping("/country/{code}")
+    public Country getCountry(@PathVariable String code) {
+
+        System.out.println("START - getCountry()");
+
+        Country country = countryService.getCountry(code);
+
+        System.out.println("END - getCountry()");
+
+        return country;
+    }
+}
