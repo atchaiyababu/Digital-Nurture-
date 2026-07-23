ed9dc61e77e9c59917f19bcdd51aa09dad8837f3
Atchaiya Babu <atchaiyababu@gmail.com>
2026-07-23 14:25:37 +0530

    Add files via upload

---
+++ Week-5/6. ReactJS-HOL/src/TrainersMock.js
@@ -0,0 +1,38 @@
+import Trainer from "./Trainer";
+
+const trainers = [
+    new Trainer(
+        1,
+        "John Smith",
+        "john@gmail.com",
+        "9876543210",
+        "Java",
+        "Spring, Hibernate"
+    ),
+    new Trainer(
+        2,
+        "Mary Johnson",
+        "mary@gmail.com",
+        "9876543211",
+        "React",
+        "ReactJS, JavaScript"
+    ),
+    new Trainer(
+        3,
+        "David Wilson",
+        "david@gmail.com",
+        "9876543212",
+        "Python",
+        "Django, Flask"
+    ),
+    new Trainer(
+        4,
+        "Emma Brown",
+        "emma@gmail.com",
+        "9876543213",
+        "Cloud",
+        "AWS, Docker"
+    )
+];
+
+export default trainers;
