09e3b187bafc56078bf9daf4993850ce5fe944c6
Atchaiya Babu <atchaiyababu@gmail.com>
2026-07-23 14:31:16 +0530

    Add files via upload

---
+++ Week-5/7. ReactJS-HOL/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import OnlineShopping from './Components/OnlineShopping';
+
+function App() {
+  return (
+    <div>
+      <OnlineShopping />
+    </div>
+  );
+}
+
+export default App;
